378d5c646990303a2e6a58b1ed30214659ed18d9
Rintu <rintuboro369@gmail.com>
2024-08-11 14:14:55 +0530

    ready for deployment

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server =  IS_PROD ? 
-    "https://video-meet-backend-0wi9.onrender.com" 
+    "https://video-meet-backend-0wi9.onrender.com"
     :
     "http://localhost:8000"
     
